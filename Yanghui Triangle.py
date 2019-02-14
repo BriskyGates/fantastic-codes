f8582ef02f1682c227830124680a5ba63f456cad
miki <2602594534@qq.com>
2019-02-14 20:11:58 +0800

    Update Yanghui Triangle.py
    新增加了一种创造杨辉三角的方法,

--- Yanghui Triangle.py
+++ Yanghui Triangle.py
@@ -1,13 +1,24 @@
 n = 0
 results = []
 
+def triangles2():
+    s = [1]
+    while True:
+        z = s
+        yield z
+        z = s + [0]
+        s = [1] + [z[i] + z[i - 1] for i in range(-n, 0)]
+
+
+
+
 def triangles():
     L = [1]
     while True:
         yield L#返回当前L列表中的元素
         L = [1] + [L[i - 1] + L[i] for i in range(1, len(L))] + [1]
 
-for t in triangles():
+for t in triangles():#或者调用triangles2()
     print(t)
     results.append(t)#把每层的列表再放入results列表中
 
