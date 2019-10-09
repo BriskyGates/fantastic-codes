d2b92512a058bd26a2d79fab1a545cb861e3a037
mj2602594534 <34669414+mj2602594534@users.noreply.github.com>
2019-10-09 11:24:28 +0800

    Update quicksort.py
    add some notes

--- quicksort.py
+++ quicksort.py
@@ -4,11 +4,12 @@ def quickSort1(list_before):
     if len(list_before)<2:
         return list_before
 
-    midprivot=list_before[len(list_before)//2]
+    midprivot=list_before[len(list_before)//2]  # 以中间元素作为基准
+    
     left_part=[x for x in list_before if x<midprivot]
     right_part=[y for y in list_before if y>midprivot]
-
     standard_point=[z for z in list_before if z==midprivot ]
+    
     return quickSort1(left_part)+standard_point+quickSort1(right_part)
 
 print(quickSort1(list_before))
@@ -16,6 +17,8 @@ print(quickSort1(list_before))
 def quickSort2(list_before):
     if len(list_before)<2:
         return list_before
+    
+    # 以列表中的第一个元素为基准
     # list_before[0:1] 是一个列表,可以与其他列表进行+运算
     return quickSort2([left_ele for left_ele in list_before[1:] if left_ele<list_before[0]])+list_before[0:1]+quickSort2([right_ele for right_ele in list_before[1:] if right_ele>=list_before[0]])
 
